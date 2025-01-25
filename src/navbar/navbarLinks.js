60721568011f7bc3a7c5910c454c7f69705d6e59
Preslav Kolev <preslavkolev18@gmail.com>
2025-01-26 01:05:00 +0200

    Added rotating gradient to register page

--- src/navbar/navbarLinks.js
+++ src/navbar/navbarLinks.js
@@ -1,11 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const navLinks = document.querySelectorAll(".navBarRight a");
-  const currentPage = window.location.pathname;
-  
-  navLinks.forEach(link => {
-    if (link.getAttribute("href") === currentPage) {
-      link.classList.add("active");
-    }
-    else link.classList.add("inactive");
-  });
+const navLinks = document.querySelectorAll(".navBarRight a");
+const currentPage = window.location.pathname;
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+  else link.classList.add("inactive");
 });
